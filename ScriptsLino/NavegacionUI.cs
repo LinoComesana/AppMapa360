9f62e363ab1ca068a26ec5508e00e0fe6253ec30
Lino José Comesaña Cebral <37588971+LinoComesana@users.noreply.github.com>
2025-06-26 08:40:47 +0200

    Reading from firebase stored JSON
    Hasta aquí:
    · Carga de mapa
    · Cargado de marcadores a partir de JSON
    · Navegación por el mapa usando AWSD y NM
    · Cada marcador apunta a una escena con visor 360 (se carga como Skybox la imagen de fondo - por ahora almacenada en local)
    · Almacén en Firebase de info de marcadores (a excepción de la imagen, sólo se almacena el atributo "imagen360", que es un identificador solamente de la imagen almacenada todavía en local)
    · Creación de escena para subir marcador (todavía sin configurar)
    · Botones de regreso a escena de mapa

---
+++ ScriptsLino/NavegacionUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavegacionUI : MonoBehaviour
+{
+    public void IrASceneAgregarUbicacion()
+    {
+        SceneManager.LoadScene("SceneAgregarUbicacion");
+    }
+
+    public void VolverAlMapa()
+    {
+        SceneManager.LoadScene("SceneMapa");
+    }
+
+
+
+
+
+}
